6164e2bef705471304bab61a2bb8c3dfdbc00b77
AdityaAtulTewari <adityaatewari@gmail.com>
2021-10-23 00:04:16 -0500

    Updated runscript to make life easier

--- latest/scripts/ckpt.sh
+++ latest/scripts/ckpt.sh
@@ -2,14 +2,15 @@
 
 GEM5_PATH=/IMPICA/gem5
 
-WORK_PATH=/out
+WORK_PATH=/work/m5_outputs
 
 U_ID=`date +"%d%b%Y%Z%H%M"`
 
-mkdir -p ${WORK_PATH}/m5_outputs/${UID}
+mkdir -p ${WORK_PATH}/m5_outputs/${U_ID}
 
 M5_PATH=/IMPICA/gem5/build/ ${GEM5_PATH}/build/ARM/gem5.opt \
-  -d ${WORK_PATH}/m5_outputs/${U_ID} \
+  -d ${WORK_PATH}/${U_ID} \
+  --listener-mode=off \
   ${GEM5_PATH}/configs/example/fs.py \
   --machine-type=VExpress_EMM64 \
   -n 4 \
@@ -18,9 +19,8 @@ M5_PATH=/IMPICA/gem5/build/ ${GEM5_PATH}/build/ARM/gem5.opt \
   --dtb-file=$KBLD/arch/arm64/boot/dts/aarch64_gem5_server.dtb \
   --kernel=$KBLD/vmlinux \
   --script /scripts/ckpt.rsc \
-  --checkpoint-at-end \
-  | tee ${WORK_PATH}/m5_outputs/${U_ID}/${U_ID}.log
+  | tee ${WORK_PATH}/${U_ID}/${U_ID}.log
 
-mkdir -p ${WORK_PATH}/m5_outputs/ckpt
-rm -rf ${WORK_PATH}/m5_outputs/ckpt
-mv ${WORK_PATH}/m5_outputs/${U_ID} ${WORK_PATH}/m5_outputs/ckpt
+mkdir -p ${WORK_PATH}/ckpt
+rm -rf ${WORK_PATH}/ckpt
+mv ${WORK_PATH}/${U_ID} ${WORK_PATH}/ckpt
